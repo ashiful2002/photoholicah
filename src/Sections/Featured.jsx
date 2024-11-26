6cab49d9a18102b3715e1c18fa077562c5cba8cb
ashiful-islam <ashifulislam2002@gmail.com>
2024-11-27 04:37:59 +0600

    second commit

--- src/Sections/Featured.jsx
+++ src/Sections/Featured.jsx
@@ -3,7 +3,7 @@ import Carousel from "react-bootstrap/Carousel";
 import { carouselItems } from "../Constants/Index";
 function Featured() {
   return (
-    <div className="" id="home">
+    <div className="container mx-auto" id="home">
       <Carousel>
         {carouselItems.map((item) => (
           <Carousel.Item>
